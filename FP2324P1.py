18be04d99a60273e8b921281a338ee83b85cd5c1
guize477 <guiandrezjose@gmail.com>
2023-10-22 16:16:11 +0100

    Trabalho final

--- FP2324P1.py
+++ FP2324P1.py
@@ -1,18 +1,23 @@
 # This is the Python script for your project
 # projeto 1
 
-def eh_territorio(arg):
+def eh_territorio(t):
     """
-    Esta função recebe um argumento e verifica se é um território válido.
+    Esta função recebe um argumento e verifica se é um território válido,
+    devolvendo um booleano em caso de verdade.
     """
-    if type(arg) is not tuple or not 0 < len(arg) < 27:
+
+    if type(t) is not tuple or not 0 < len(t) < 27:
         return False
+    
+    # guarda a altura do território, se o primeiro argumento for iteravel
     try:
-        altura = len(arg[0])
+        altura = len(t[0])
     except TypeError:
         return False
-        
-    for coluna in arg:
+    
+    # valida os tuplos e os seus elementos
+    for coluna in t:
         if (type(coluna) is not tuple or not 0 < len(coluna) < 100
             or len(coluna) != altura):
             return False
@@ -25,36 +30,34 @@ def obtem_ultima_intersecao(t):
     """
     Esta função recebe um território e devolve a última interseção.
     """
-    if not eh_territorio(t): # verificar se o território é válido
-        return False
-    # devolver a última coordenada
     return (chr(len(t) + 64), len(t[0]))
 
-def eh_intersecao(arg):
+def eh_intersecao(intersecao):
     """
     Esta função recebe um argumento e verifica se é uma interseção válida,
     ou seja, se é um tuplo com dois elementos, sendo o primeiro uma letra 
     maiúscula e o segundo um número entre 1 e 99.
     """
-    if not isinstance(arg, tuple): # verificar se o argumento é um tuplo
+    if type(intersecao) is not tuple: 
         return False
-    if (len(arg) != 2 or not isinstance(arg[0], str) # verifica se as outras condições são válidas
-        or not isinstance(arg[1], int) or not arg[0].isupper() 
-        or not 1 <= arg[1] <= 99 or len(arg[0]) != 1):
+    
+    if (len(intersecao) != 2 or type(intersecao[0]) is not str 
+        or type(intersecao[1]) is not int or not intersecao[0].isupper()
+        or not 1 <= intersecao[1] <= 99 or len(intersecao[0]) != 1):
         return False
     
     return True
 
 def eh_intersecao_valida(t, i):
     """
-    Esta função recebe um território e uma interseção e verifica se a 
-    interseção pode pertencer ao território.
+    Esta função recebe um território e uma interseção e verifica a
+    sua compatibilidade, devolvendo um booleano em caso de verdade.
     """
-    # verificar se o território e a interseção são válidos
+
     if not eh_territorio(t) or not eh_intersecao(i): 
         return False
     
-    # Verificar se interseção não ultrapassa limites do território
+    # Verifica se interseção não excede o território
     if ord(i[0]) - 64 > len(t) or i[1] > len(t[0]):
         return False
     
@@ -63,12 +66,14 @@ def eh_intersecao_valida(t, i):
 def eh_intersecao_livre(t, i):
     """
     Esta função recebe um território e uma interseção e verifica se a 
-    interseção tem valor de 0 (se é livre), devolvendo um booleano.
+    interseção tem valor de 0 (se é livre), devolvendo um booleano em 
+    caso de verdade.
     """
-    # verificar se o território e a interseção são compatíveis
+
     if not eh_intersecao_valida(t, i):
         return False
-    # verificar se a coordenada corresponde a um 0 ou 1
+    
+    # devolve o booleano correspondente ao valor da interseção
     return t[ord(i[0]) - 65][i[1] - 1] == 0
 
 def obtem_intersecoes_adjacentes(t, i):
@@ -76,37 +81,25 @@ def obtem_intersecoes_adjacentes(t, i):
     Esta funcão recebe um território e uma interseção e devolve um tuplo com as
     interseções adjacentes à interseção dada.
     """
-    if not eh_intersecao_valida(t, i): # verificar se a interseção é válida
-        return False
-    adjacentes = ()
     # verificar as 4 possíveis interseções adjacentes
-    if eh_intersecao_valida(t, (i[0], i[1] - 1)):
-        adjacentes = adjacentes + (((i[0], i[1] - 1)),)
+    adjacente1 = (i[0], i[1] - 1)
+    adjacente2 = (chr(ord(i[0]) - 1), i[1])
+    adjacente3 = (chr(ord(i[0]) + 1), i[1])
+    adjacente4 = (i[0], i[1] + 1)
+    novos_adjacentes = (adjacente1, adjacente2, adjacente3, adjacente4)
 
-    if eh_intersecao_valida(t, (chr(ord(i[0]) - 1), i[1])):
-        adjacentes += (((chr(ord(i[0]) - 1), i[1]),))
-        
-    if eh_intersecao_valida(t, (chr(ord(i[0]) + 1), i[1])):
-        adjacentes += (((chr(ord(i[0]) + 1), i[1]),))
+    # adicionar as interseções válidas ao tuplo adjacentes
+    return tuple(adj for adj in novos_adjacentes 
+                if eh_intersecao_valida(t, adj))
 
-    if eh_intersecao_valida(t, (i[0], i[1] + 1)):
-        adjacentes += (((i[0], i[1] + 1)),)
-
-    return adjacentes
-
-def ordena_intersecoes(arg):
+def ordena_intersecoes(intersecao):
     """
     Esta função rebe um tuplo com interseções e devolve um tuplo com as 
-    intersesções ordenadas.
+    mesmas intersesções ordenadas.
     """
-    if type(arg) is not tuple: # verifica se o argumento é um tuplo
-        return False
-    for item in arg: # itera sobre as interseções para verificar se são válidas
-        if type(item) is not tuple and not eh_intersecao(item):
-            raise ValueError('argumentos inválidos')
     # ordena as interseções, usando um lambda para dar prioridade à letra,
     # ou seja, ao segundo elemento do tuplo da interseção
-    return tuple(sorted(arg, key=lambda x: (x[1], x[0])))
+    return tuple(sorted(intersecao, key=lambda x: (x[1], x[0])))
 
 def territorio_para_str(t):
     """
@@ -116,7 +109,7 @@ def territorio_para_str(t):
     if not eh_territorio(t): # verificar se o território é válido
         raise ValueError('territorio_para_str: argumento invalido')
     
-    def digits(num): # função auxiliar que conta o número de dígitos de um número
+    def digits(num): # função que devolve o número de dígitos de um int
         digitos = 0
         while num != 0:
             digitos += 1
@@ -131,12 +124,14 @@ def territorio_para_str(t):
     territorio += '\n'
 
     for j in range(altura): # itera por n linhas sendo n o valor da altura
-        territorio = territorio + (2 - digits(altura - j)) * " " + f"{altura - j} " 
+        espacamento = 2 - digits(altura - j)
+        territorio = territorio + " " * espacamento + f"{altura - j} " 
         for x in range(largura): # iteram por n colunas sendo n o valor da largura
             intersecao = '. ' if t[x][altura - j - 1] == 0 else 'X '
             territorio += intersecao
-        territorio = territorio + (2 - digits(altura - j)) * " " + f"{altura - j}\n" 
+        territorio = territorio + " " * espacamento + f"{altura - j}\n" 
     territorio += "  "
+    
     for i in range(largura): # adiciona a ulitma linha com as letras
         territorio += f" {chr(65 + i)}"
     return territorio
@@ -153,11 +148,11 @@ def obtem_cadeia(t, i):
     # Função para obter interseções adjacentes com mesmo estado de ocupação
     def adjacentes_tipo(t, i): 
         adjacentes = obtem_intersecoes_adjacentes(t, i)
-        adjacentes_livres = tuple(adjacente for adjacente in adjacentes 
-                                  if eh_intersecao_livre(t, adjacente) == 
-                                  eh_intersecao_livre(t, i))
+        adj_mesmo_tipo = tuple(adj for adj in adjacentes 
+                                if eh_intersecao_livre(t, adj) == 
+                                eh_intersecao_livre(t, i))
         
-        return adjacentes_livres
+        return adj_mesmo_tipo
     
     cadeia = adjacentes_tipo(t, i) + (i,) 
 
@@ -170,11 +165,11 @@ def obtem_cadeia(t, i):
 
             # cria um tuplo com as interseções adjacentes que ainda não estão 
             # na cadeia nem no tuplo novos_adjacentes para evitar repetições
-            aux = tuple(adj for adj in adjacentes_tipo(t, adjacente) 
+            adicionados = tuple(adj for adj in adjacentes_tipo(t, adjacente) 
                         if adj not in cadeia and adj not in novos_adjacentes)
-            novos_adjacentes += aux
+            novos_adjacentes += adicionados
 
-            if aux: # define o valor de adicionado se o tuplo aux não for vazio
+            if adicionados: # se adicionados não for vazio, adiciona-se True
                 adicionado = True
 
         cadeia += novos_adjacentes
@@ -187,7 +182,7 @@ def obtem_vale(t, i):
     e devolve um tuplo com as interseções vazias adjacentes às montanhas que
     pertencem à cadeia da interseção dada.
     """
-    # verificar se os argumentos são válidos
+    # valida os argumentos
     if not eh_intersecao_valida(t, i) or eh_intersecao_livre(t, i):
         raise ValueError("obtem_vale: argumentos invalidos")
     
@@ -203,7 +198,7 @@ def obtem_vale(t, i):
     cadeia = obtem_cadeia(t, i)
     vales = ()
     for montanha in cadeia: # itera sobre as interseções da cadeia
-        # adiciona as interseções adjacentes com o estado de ocupação diferente
+        # adiciona as interseções vazias adjacentes às montanhas da cadeia
         vales += tuple(vale for vale in adjacentes_tipo_diferente(t, montanha) 
                        if vale not in vales)
 
@@ -214,7 +209,7 @@ def verifica_conexao(t, i1, i2):
     Esta função recebe um território e duas interseçõese e verifica se as
     interseções pertencem à mesma cadei, devolvendo um booleano.
     """
-    # verificar se os argumentos são válidos
+    # validar os argumentos
     if not eh_intersecao_valida(t, i1) or not eh_intersecao_valida(t, i2):
         raise ValueError('verifica_conexao: argumentos invalidos')
 
@@ -226,7 +221,7 @@ def calcula_numero_montanhas(t):
     Esta função recebe um território e devolve um inteiro com o número de
     montanhas existentes no território.
     """
-    # verificar se o argumento é válido
+    # valida o argumento
     if not eh_territorio(t):
         raise ValueError("calcula_numero_montanhas: argumento invalido")
     
@@ -241,14 +236,14 @@ def calcula_numero_cadeias_montanhas(t):
     Esta função recebe um território e devolve um inteiro com o número de
     cadeias diferentes de montanhas existentes no território.
     """
-    # verificar se o argumento é válido
+    # valida o argumento
     if not eh_territorio(t):
         raise ValueError("calcula_numero_cadeias_montanhas: argumento invalido")
     cadeias = ()
     contador = 0
+
     # vai adicionando as cadeias de montanhas ao tuplo cadeias e conta o número
     # de vezes que isso acontece
-
     for y in range(len(t[0])):
         for x in range(len(t)):
             if t[x][y] == 1 and (chr(65 + x), y + 1) not in cadeias:
@@ -267,9 +262,9 @@ def calcula_tamanho_vales(t):
     # itera sobre as interseções do território e adiciona as interseções vazias
     for y in range(len(t[0])):
         for x in range(len(t)):
-            if not eh_intersecao_livre(t, (chr(65 + x), y + 1)):
-                novo_vale = obtem_vale(t, (chr(65 + x), y + 1))
+            intersecao = (chr(65 + x), y + 1)
+            if not eh_intersecao_livre(t, intersecao):
+                novo_vale = obtem_vale(t, intersecao)
                 vales += tuple(vale for vale in novo_vale if vale not in vales)
     # devolve o número de interseções vazias que pertencem a vales
     return len(vales)
-
